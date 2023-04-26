9bdba63e8a8ff4f836d9668482a484bd6d06a0df
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-04-25 18:24:32 -0600

    Data Provider, Data Set

---
+++ ADOdotNet/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ADOdotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ADO.NET Data Provider");
+
+            string connectionString = "Data Source=localhost;" +
+                "Initial Catalog=Northwind;" +
+                "trusted_connection=true;" +
+                "Encrypt=False;";
+
+            string query = "SELECT Customers.CustomerID," +
+                    "Customers.CompanyName," +
+                    "Customers.ContactTitle " +
+                "FROM Customers";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0},{1},{2}",
+                            reader[0], reader[1], reader[2]);
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ ADOdotNetDataSets/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ADOdotNetDataSets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ta = new DataSet1TableAdapters.CustomersTableAdapter();
+            var dt = ta.GetData();
+
+            foreach (DataSet1.CustomersRow row in dt.Rows)
+            {
+                Console.WriteLine("{0},{1},{2}",
+                    row.CustomerID, row.CompanyName, row.ContactTitle);
+            }
+            dt.Dispose();
+            ta.Dispose();
+        }
+    }
+}
